5cc68940d7cec79741411b2aad21b9d0c54cd1c6
seewod <dongdanyan@outlook.com>
2026-03-17 21:56:25 +0800

    fix: 网络相关配置（docker 网桥的，后面还要修改

--- back-end/config/db.config.js
+++ back-end/config/db.config.js
@@ -1,6 +1,7 @@
 const { MongoClient } = require("mongodb");
 // Replace the uri string with your connection string.
-const uri = "mongodb://127.0.0.1:27017";
+const mongoHost = process.env.MONGO_HOST || "127.0.0.1";
+const uri = `mongodb://${mongoHost}:27017`;
 const client = new MongoClient(uri);
 
 const database = client.db('BlaBlaLog');
